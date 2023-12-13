c6b4fafdb85f5547ac19696bd288929f023286a5
VillageWars <vivid.tree0984@fastmail.com>
2023-12-13 15:29:09 +0100

    Last commit before uploading

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
-
 import asyncio
 import json
 import secrets
 import threading
 from flask import Flask, request, session, url_for, redirect
+import os
+import signal
 
 app = Flask(__name__)
 
@@ -90,8 +90,14 @@ async def handler(websocket):
         
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
